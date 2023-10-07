f52998a79c66880ec0d2017876f9ac015528bf83
Yey007 <yey007tech@gmail.com>
2023-10-07 19:48:42 -0400

    clean up

--- app2/src/pages/Home.tsx
+++ app2/src/pages/Home.tsx
@@ -1,23 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage } from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonContent fullscreen></IonContent>
     </IonPage>
   );
 };
