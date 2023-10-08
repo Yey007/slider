e0bb25ca0816aec91a196ff52e8924af188ac782
Yey007 <yey007tech@gmail.com>
2023-10-07 20:31:20 -0400

    move app

--- app/android/app/src/main/kotlin/com/example/slider_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.slider_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { IonContent, IonPage } from "@ionic/react";
+import "./Home.css";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen></IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
