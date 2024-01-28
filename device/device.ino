25935cbe1258d01e550647a99e0c249f3716c10a
Yey007 <yeeeeeeey007@gmail.com>
2024-01-28 16:05:21 -0500

    Working VACTUAL?

--- device/device.ino
+++ device/device.ino
@@ -10,7 +10,7 @@
 #define RX_PIN 7
 #define TX_PIN 6
 
-#define R_SENSE 0.15f // Not certain what this should be
+#define R_SENSE 0.11f // Not certain what this should be
 
 TMC2208Stepper driver = TMC2208Stepper(RX_PIN, TX_PIN, R_SENSE);
 
@@ -28,16 +28,16 @@ void setup()
     driver.begin(); // enables uart interface and sets driver to use register microsteps
     driver.toff(1); // Enables driver in software (can be any value except 0 for StealthChop)
 
-    driver.internal_Rsense(true); // use internal sense resistors
-    driver.rms_current(250, 0.2); // automatically calculates irun and ihold based on rms current and hold multiplier
-    driver.iholddelay(10);        // some ramp down time to hold current (view docs for time calculation)
-    driver.TPOWERDOWN(255);       // some time until ramp down begins (view docs for time calculation, this is around 5.6 seconds)
-    driver.vsense(false);         // full voltage
+    driver.internal_Rsense(false); // don't use internal sense resistors (board should have some)
+    driver.rms_current(400, 0.2);  // automatically calculates irun and ihold based on rms current and hold multiplier
+    driver.iholddelay(10);         // some ramp down time to hold current (view docs for time calculation)
+    driver.TPOWERDOWN(255);        // some time until ramp down begins (view docs for time calculation, this is around 5.6 seconds)
+    driver.vsense(false);
 
     driver.en_spreadCycle(false); // use StealthChop
 
-    driver.pwm_autoscale(true); // automatic motor voltage scaling based on tuning run. Can be switched to velocity based voltage scaling.
-    driver.pwm_autograd(true);  // automatic motor voltage ramp up based on tuning run. Need to be switched off for velocity based scaling?
+    driver.pwm_autoscale(false); // velocity based voltage scaling
+    driver.pwm_autograd(false);  // off for above
 
     driver.tbl(2); // not really sure what this does but recommended for most applications
 
@@ -50,76 +50,13 @@ void setup()
     Serial.print("Microsteps: ");
     Serial.println(ms);
 
-    // autoTune();
-
-    Bezier curve = Bezier(
-        BezierEndpoint(0, 0),
-        0,
-        200,
-        BezierEndpoint(200, 2000));
-
-    run(curve, millis());
+    driver.VACTUAL(10);
+    Serial.println("Start VACTUAL");
 }
 
 void loop()
 {
-}
-
-void autoTune()
-{
-    uint8_t ihold_before = driver.ihold();
-    uint8_t irun_before = driver.irun();
-
-    // temporarily set ihold and irun to max for tuning
-    driver.ihold(31);
-    driver.irun(31);
-
-    delay(100); // wait for auto tuning step 1 (standstill) to complete
-
-    // TODO: probably replace with a bezier curve or velocity control once we know everything is working
-    const uint32_t initial_speed = 100;
-    const uint32_t max_speed = 500;
-    const uint32_t max_velo_iterations = 4000;
-
-    bool current_step = LOW;
-    uint32_t current_speed = initial_speed;
-
-    while (current_speed < max_speed)
-    {
-        digitalWrite(STEP_PIN, current_step);
-        current_step = !current_step;
-        microstepsPerSecondToDelay(current_speed);
-        current_speed++; // This isn't really constant acceleration, but doesn't matter much at these speeds.
-    }
-
-    for (int i = 0; i < max_velo_iterations; i++)
-    {
-        digitalWrite(STEP_PIN, current_step);
-        current_step = !current_step;
-        microstepsPerSecondToDelay(current_speed);
-    }
-
-    while (current_speed > initial_speed)
-    {
-        digitalWrite(STEP_PIN, current_step);
-        current_step = !current_step;
-        microstepsPerSecondToDelay(current_speed);
-        current_speed--;
-    }
-
-    driver.ihold(ihold_before);
-    driver.irun(irun_before);
-}
-
-void microstepsPerSecondToDelay(uint32_t microsteps_per_second)
-{
-    // period: 1/f (seconds per microstep)
-    double period_seconds = 1.0 / microsteps_per_second;
-    uint32_t period_micros = period_seconds * 1000000;
-
-    unsigned long start_micros = micros();
-    while (micros() - start_micros < period_micros)
-        continue;
+    driver.VACTUAL(2000);
 }
 
 dist_t current_ticks = 0;
